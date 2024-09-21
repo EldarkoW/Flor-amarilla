4265ac7273e2479489d2ccf8edd1f7c619fe697c
EldarkoW <wilson_rene00@hotmail.com>
2024-09-20 21:37:31 -0500

    Update astro.config.mjs

--- .github/astro.config.mjs
+++ .github/astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://joacohbc.github.io',
-  base: '/flores-amarillas'
+  site: 'https://EldarkoW.github.io',
+  base: '/para-mi-Shuyana-hermosa-preciosa'
 });
